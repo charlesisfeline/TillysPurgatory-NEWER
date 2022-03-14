eeb0607127fb45cc9b042f1dd8883e7d7e4769e8
Payten <94698464+PDL2CGamejolt@users.noreply.github.com>
2022-03-14 10:10:30 -0500

    Update toomuch.lua

--- example_mods/stages/toomuch.lua
+++ example_mods/stages/toomuch.lua
@@ -3,6 +3,7 @@ function onCreate()
 	makeLuaSprite('toomuch', 'toomuch', -600, -300);
 	setScrollFactor('toomuch', 0.9, 0.9);
 	addLuaSprite('toomuch', false);
+        addGlitchEffect('toomuch',2,2)
 	
 	close(true); --For performance reasons, close this script once the stage is fully loaded, as this script won't be used anymore after loading the stage
 end
