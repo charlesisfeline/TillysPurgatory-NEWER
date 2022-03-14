40a0553cdb36e5c1d4c2a37e3fcd1bb169aed3bb
Payten <94698464+PDL2CGamejolt@users.noreply.github.com>
2022-03-14 10:13:08 -0500

    Update golden.lua

--- example_mods/stages/golden.lua
+++ example_mods/stages/golden.lua
@@ -3,6 +3,7 @@ function onCreate()
 	makeLuaSprite('golden', 'golden', -600, -300);
 	setScrollFactor('golden', 0, 0);
 	addLuaSprite('golden', false);
+	addGlitchEffect('golden',2,2)
 	
 	close(true); --For performance reasons, close this script once the stage is fully loaded, as this script won't be used anymore after loading the stage
 end
