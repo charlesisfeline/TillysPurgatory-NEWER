b813fb19385dd62bf84a8737202082d973dcadeb
Payten <94698464+PDL2CGamejolt@users.noreply.github.com>
2022-03-14 10:10:58 -0500

    Update theating.lua

--- example_mods/stages/theating.lua
+++ example_mods/stages/theating.lua
@@ -3,6 +3,7 @@ function onCreate()
 	makeLuaSprite('theating', 'theating', -600, -300);
 	setScrollFactor('theating', 0, 0);
 	addLuaSprite('theating', false);
+	addGlitchEffect('theating',2,2)
 	
 	close(true); --For performance reasons, close this script once the stage is fully loaded, as this script won't be used anymore after loading the stage
 end
