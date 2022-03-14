446fcacf3e0dba6dff27b55bee7d4dd80853b8cb
Payten <94698464+PDL2CGamejolt@users.noreply.github.com>
2022-03-14 10:12:05 -0500

    Update pogface.lua

--- example_mods/stages/pogface.lua
+++ example_mods/stages/pogface.lua
@@ -3,6 +3,7 @@ function onCreate()
 	makeLuaSprite('pogface', 'pogface', -600, -300);
 	setScrollFactor('pogface', 0, 0);
 	addLuaSprite('pogface', false);
+	addGlitchEffect('pogface',2,2)
 	
 	close(true); --For performance reasons, close this script once the stage is fully loaded, as this script won't be used anymore after loading the stage
 end
